26b71da0456a0b59d8296a9ce808ac948676a0a7
Caleb Kang <bhk811@gmail.com>
2019-02-04 15:00:30 -0800

    Updates

---
+++ precache-manifest.702f5d940c4247cbc51ab1460acf5887.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "debfbcf2ed160f2ed4c7",
+    "url": "/clicky-game/static/css/main.a5b58c49.chunk.css"
+  },
+  {
+    "revision": "debfbcf2ed160f2ed4c7",
+    "url": "/clicky-game/static/js/main.debfbcf2.chunk.js"
+  },
+  {
+    "revision": "9c49b1657f794805e3dd",
+    "url": "/clicky-game/static/js/1.9c49b165.chunk.js"
+  },
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "7042af67ee4d681c470998ace167ab84",
+    "url": "/clicky-game/index.html"
+  }
+];
